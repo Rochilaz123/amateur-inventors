495eb53de72e2880441aa62b495fafcced4e2f0f
Rachel Lazarus <rochilazarus@gmail.com>
2024-06-26 15:50:28 +0000

    Generic Ideas view on home page.

--- forum/views.py
+++ forum/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from.models import Idea
 
 # Create your views here.
-def my_forum(request):
-    return HttpResponse("Hello, Blog!")
+
+class IdeaList(generic.ListView):
+    queryset = Idea.objects.all()
+    template_name = "idea_list.html"
