dc3eace7dddd6d310586766bb0ccdbe2eeffb1fd
Rachel Lazarus <rochilazarus@gmail.com>
2024-06-26 14:13:33 +0000

    Secure secret key

--- amateurinventors/settings.py
+++ amateurinventors/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@&23k^d****d-ci9hqdimwkm5&914%*z+40a1a7)m%y)z=rx*9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-rochilaz123-amateurinve-wpvbpgd8e39.ws.codeinstitute-ide.net',
