c167d323ce9d24a89ca1a36d021704ddc11bfa41
Rachel Lazarus <rochilazarus@gmail.com>
2024-06-27 00:47:47 +0000

    idea_detail page displaying idea

--- forum/urls.py
+++ forum/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.IdeaList.as_view(), name='home'),
-    path('<slug:slug>/', views.idea_detail, name='post_detail'),
+    path('<slug:slug>/', views.idea_detail, name='idea_detail'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from.models import Idea
 
@@ -7,3 +7,14 @@ from.models import Idea
 class IdeaList(generic.ListView):
     queryset = Idea.objects.all().order_by('-created_on')
     template_name = "forum/index.html"
+
+
+def idea_detail(request, slug):
+    queryset = Idea.objects.all()
+    idea = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "forum/idea_detail.html",
+        {"idea": idea},
+    )
